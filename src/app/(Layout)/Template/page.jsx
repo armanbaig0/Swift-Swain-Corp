8873cbd11e08fdab3e052cbf6a54d74a40205a13
Arman Baig <armanbaig565@gmail.com>
2025-09-04 23:09:55 +0500

    Updated Landing Page

---
+++ src/app/(Layout)/Template/page.jsx
@@ -0,0 +1,14 @@
+'use client';
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+
+function Template (){
+    return(
+        <>
+        <Header />
+        <Footer />
+        </>
+    );
+}
+
+export default Template
---
+++ src/app/components/header.jsx
@@ -0,0 +1,16 @@
+'use client'
+import { Typography } from "@mui/material";
+import { Box } from "@mui/material";
+
+function Header (){
+
+    return (
+        <>
+          <Box>
+            <Typography>This is Arman Header</Typography>
+          </Box>
+        </>
+    );
+}
+
+export default Header;
